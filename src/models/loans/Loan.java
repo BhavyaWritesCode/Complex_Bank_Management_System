6215b19dd4a0b415f0e19aac31edf8ad313c39a1
Bhavya Saini <bhavyasaini06@gmail.com>
2025-07-10 23:16:26 +0530

    Update Loan.java

--- src/models/loans/Loan.java
+++ src/models/loans/Loan.java
@@ -1,17 +1,18 @@
 package models.loans;
+
 import java.util.UUID;
 import java.time.LocalDate;
 
 public abstract class Loan {
-    protected String loanID;
+    protected String loanId;
     protected double amount;
     protected double interestRate;
     protected int durationMonths;
     protected double monthlyEMI;
     protected LocalDate startDate;
 
-    public Loan(double amount, double interestRate, int durationMonths){
-        this.loanID - UUID.randomUUID().toString();
+    public Loan(double amount, double interestRate, int durationMonths) {
+        this.loanId = UUID.randomUUID().toString();  // unique ID
         this.amount = amount;
         this.interestRate = interestRate;
         this.durationMonths = durationMonths;
@@ -19,6 +20,27 @@ public abstract class Loan {
         this.monthlyEMI = calculateEMI();
     }
 
+    // Abstract method to override in sub-classes
     public abstract String getLoanType();
-    
+
+    // EMI calculation formula
+    public double calculateEMI() {
+        double monthlyRate = interestRate / 12 / 100;
+        double emi = (amount * monthlyRate * Math.pow(1 + monthlyRate, durationMonths)) /
+                     (Math.pow(1 + monthlyRate, durationMonths) - 1);
+        return Math.round(emi * 100.0) / 100.0;
+    }
+
+    // Getters
+    public String getLoanId() { return loanId; }
+    public double getAmount() { return amount; }
+    public double getInterestRate() { return interestRate; }
+    public int getDurationMonths() { return durationMonths; }
+    public double getMonthlyEMI() { return monthlyEMI; }
+    public LocalDate getStartDate() { return startDate; }
+
+    // File export format
+    public String toFileString(String username) {
+        return username + "|" + getLoanType() + "|" + loanId + "|" + amount + "|" + interestRate + "|" + durationMonths + "|" + monthlyEMI + "|" + startDate;
+    }
 }
