5b1418ae1787b870c5c32245ca3654d8fe43eece
Bhavya Saini <bhavyasaini06@gmail.com>
2025-07-11 22:28:09 +0530

    Update personalLoan

--- src/models/loans/PersonalLoan.java
+++ src/models/loans/PersonalLoan.java
@@ -1,5 +1,14 @@
 package models.loans;
 
-public class PersonalLoan {
-    
+public class PersonalLoan extends Loan {
+
+    public PersonalLoan(double amount, int durationMonths) {
+        
+        super(amount, 12.0, durationMonths);
+    }
+
+    @Override
+    public String getLoanType() {
+        return "Personal Loan";
+    }
 }
