e720e4cb4c59447729caa2fde8ede8cf58f9574a
Bhavya Saini <bhavyasaini06@gmail.com>
2025-07-11 22:24:31 +0530

    Update HomeLoan

--- src/models/loans/HomeLoan.java
+++ src/models/loans/HomeLoan.java
@@ -1,5 +1,16 @@
 package models.loans;
 
-public class HomeLoan {
+public class HomeLoan extends Loan{
+
+    public HomeLoan(double amount, int durationMonths){
+
+        super(amount, 7.5, durationMonths); 
+
+    }
+
+    @Override
+    public String getLoanType() {
+        return "Home Loan";
+    }
     
 }
