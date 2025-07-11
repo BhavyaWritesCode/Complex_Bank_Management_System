ba48f91d19df4111d5bfa76b4867357b9007305a
Bhavya Saini <bhavyasaini06@gmail.com>
2025-07-11 22:21:38 +0530

    Update EduLoan

--- src/models/loans/EduLoan.java
+++ src/models/loans/EduLoan.java
@@ -1,5 +1,13 @@
 package models.loans;
 
-public class EduLoan {
+public class EduLoan extends Loan{
+    public EduLoan(double amount, int durationMonths) {
+        super(amount,8.0, durationMonths); 
+    }
+
+    @Override
+    public String getLoanType() {
+        return "Education Loan";
+    }
     
 }
