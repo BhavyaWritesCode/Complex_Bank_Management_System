b7cffa2c11aa058fc7673c27d4f73df23732c456
Bhavya Saini <bhavyasaini06@gmail.com>
2025-07-18 23:33:20 +0530

    Update business loan

--- src/models/loans/BusinessLoan.java
+++ src/models/loans/BusinessLoan.java
@@ -1,5 +1,13 @@
 package models.loans;
 
-public class BusinessLoan {
-    
+public class BusinessLoan extends Loan {
+
+    public BusinessLoan(double amount, int durationMonths) {
+        super(amount, 13.5, durationMonths);
+    }
+
+    @Override
+    public String getLoanType() {
+        return "Business Loan";
+    }
 }
