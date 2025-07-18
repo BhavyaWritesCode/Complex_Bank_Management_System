e0533c9badfee6643ef0ece1bd759523d0a5641a
Bhavya Saini <bhavyasaini06@gmail.com>
2025-07-18 23:33:04 +0530

    Update Vehicle loan

--- src/models/loans/VehicleLoan.java
+++ src/models/loans/VehicleLoan.java
@@ -1,5 +1,13 @@
 package models.loans;
 
-public class VehicleLoan {
-    
+public class VehicleLoan extends Loan {
+
+    public VehicleLoan(double amount, int durationMonths) {
+        super(amount, 10.0, durationMonths);
+    }
+
+    @Override
+    public String getLoanType() {
+        return "Vehicle Loan";
+    }
 }
