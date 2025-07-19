ae0bbeb09076a87442d4c25e3242138c0645c1cd
Bhavya Saini <bhavyasaini06@gmail.com>
2025-07-19 23:09:14 +0530

    New File

--- src/models/cards/Card.java
+++ src/models/cards/Card.java
@@ -1,5 +1,44 @@
 package models.cards;
 
-public class Card {
-    
+import java.time.LocalDate;
+
+public abstract class Card {
+    protected String cardNumber;
+    protected String holderName;
+    protected LocalDate issueDate;
+    protected LocalDate expiryDate;
+
+    public Card(String cardNumber, String holderName) {
+        this.cardNumber = cardNumber;
+        this.holderName = holderName;
+        this.issueDate = LocalDate.now();
+        this.expiryDate = issueDate.plusYears(5); // Valid for 5 years
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public abstract String getCardType();
+
+    @Override
+    public String toString() {
+        return "CardType: " + getCardType() + 
+               ", Number: " + cardNumber +
+               ", Holder: " + holderName +
+               ", Issue Date: " + issueDate +
+               ", Expiry Date: " + expiryDate;
+    }
 }
