2623aa638fade404c5dfa6f46d3d1fcd5820440c
Bhavya Saini <bhavyasaini06@gmail.com>
2025-07-25 23:16:00 +0530

    added methods which was causing error

--- utils/InterestRates.java
+++ utils/InterestRates.java
@@ -20,31 +20,42 @@ public class InterestRates {
         savingsRates.put("RecurringDeposit", 6.2);
     }
 
-    public static double getLoanInterestRate(String loanType) {
-        return loanRates.getOrDefault(loanType, 0.0);
+    public static double getRate(String type) {
+        if (loanRates.containsKey(type)) return loanRates.get(type);
+        if (savingsRates.containsKey(type)) return savingsRates.get(type);
+        return 0.0;
     }
 
-    public static double getSavingsInterestRate(String type) {
-        return savingsRates.getOrDefault(type, 0.0);
-    }
-
-    public static void updateLoanInterestRate(String loanType, double rate) {
-        if (rate > 0) {
-            loanRates.put(loanType, rate);
-        }
-    }
-
-    public static void updateSavingsInterestRate(String type, double rate) {
-        if (rate > 0) {
+    public static void updateRate(String type, double rate) {
+        if (rate <= 0) return;
+        if (loanRates.containsKey(type)) {
+            loanRates.put(type, rate);
+        } else if (savingsRates.containsKey(type)) {
             savingsRates.put(type, rate);
         }
     }
 
-    public static Map<String, Double> getAllLoanRates() {
-        return new HashMap<>(loanRates);
+    public static Map<String, Double> getAllRates() {
+        Map<String, Double> allRates = new HashMap<>();
+        allRates.putAll(loanRates);
+        allRates.putAll(savingsRates);
+        return allRates;
     }
 
-    public static Map<String, Double> getAllSavingsRates() {
-        return new HashMap<>(savingsRates);
+    public static boolean isLoanType(String type) {
+        return loanRates.containsKey(type);
+    }
+
+    public static boolean isSavingsType(String type) {
+        return savingsRates.containsKey(type);
+    }
+
+
+    public static double getLoanInterestRate(String loanType) {
+        return loanRates.getOrDefault(loanType, 0.0);
+    }
+
+    public static double getSavingsInterestRate(String savingsType) {
+        return savingsRates.getOrDefault(savingsType, 0.0);
     }
 }
